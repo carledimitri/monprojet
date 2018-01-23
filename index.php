d7d6b2858b51a9f9599c2aa68433f714476f0a53
carledimitri <mouelecarl@gmail.com>
2018-01-23 15:15:19 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,231 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+  
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet"  href="bootstrap.min.css">
+ 
+  <link rel="stylesheet"  href="stylecss.css">
+<style type="text/css">
+    .bs-example{
+     margin: 20PX;
+    }
+  .contenair-fluid{
+  padding: 0;
+}
+</style>
+</head> 
+<body>
+    <div class=" bs-example ">
+       <div class="bs-example ">
+     <nav class="navbar navbar-inverse red">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="#" class="navbar-brand">Brand</a>
+        </div>
+        <!-- Collection of nav links, forms, and other content for toggling -->
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li class="dropdown">
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Messages <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Inbox</a></li>
+                        <li><a href="#">Drafts</a></li>
+                        <li><a href="#">Sent Items</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Trash</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <form class="navbar-form navbar-left">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                    <span class="input-group-btn">
+                        <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+                    </span>
+                </div>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+          <div class="col-md-8 col-md-offset-2"> 
+              <div class="bs-example">
+                <div id="myCarousel" class="carousel slide " data-ride="carousel">
+                  <!-- Carousel indicators -->
+                  <ol class="carousel-indicators">
+                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                      <li data-target="#myCarousel" data-slide-to="1"></li>
+                      <li data-target="#myCarousel" data-slide-to="2"></li>
+                  </ol>   
+                  <!-- Wrapper for carousel items -->
+                  <div class="carousel-inner"  style="border-radius: 12px;">
+                      <div class="item active">
+                          <img  style="border-radius: 8px;" src="cap2.png" alt="First Slide">
+                      </div>
+                       <div class="item">
+                          <img  style="border-radius: 8px;" src="cap3.png" alt="Third Slide">
+                      </div>
+                       <div class="item ">
+                          <img  style="border-radius: 8px;" src="cap4.png" alt="First Slide">
+                      </div>
+                       <div class="item">
+                          <img  style="border-radius: 8px;" src="cap5.png" alt="Second Slide">
+                      </div>
+                      <div class="item">
+                          <img  style="border-radius: 8px;"  src="cap1.png" alt="Second Slide">
+                      </div>
+                      <div class="item">
+                          <img  style="border-radius: 8px;" src="img2.jpg" alt="Third Slide">
+                      </div>
+                  </div>
+                  <!-- Carousel controls -->
+                  <a class="carousel-control left" href="#myCarousel" data-slide="prev">
+                      <span class="glyphicon glyphicon-chevron-left"></span>
+                  </a>
+                  <a class="carousel-control right" href="#myCarousel" data-slide="next">
+                      <span class="glyphicon glyphicon-chevron-right"></span>
+                  </a>
+            </div>
+          </div>
+       </div>
+      </div>
+        <div class="container">
+          <div class="container ">
+              <div  class="jumbotron ">
+                <h1 class="text-center">Bienvenu sur mon site</h1>      
+                <p class="text-center">Ceci est mon premier site officiel pour ma future application  creer a base de bootstrap merci...</p>
+              </div>   
+          </div>
+          <div class="responsive">
+            <div class="row">
+               <article class="col-md-3 col-lg-3 col-xs-12 col-lg-12">
+               
+                  <h1>Mes etude</h1>
+                  <img src="paris.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236">
+                  <p>
+                    This is a prerequisite stage all students are required to complete. You can choose the Javascript track or the Java track. Both languages allow you to develop business applications, mobile apps and desktop solutions. To load the contents of the track you selected, press the continue button and confirm.
+                     
+                  </p>
+                  <a class="btn btn-info btn-ls" href="youtube.com" target="blank">inform</a>
+              </article>
+               <article class="col-md-3 col-lg-3 col-xs-12 col-lg-12">
+              
+                  <h1>Mes etude</h1>
+                  <img src="newyork.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236">
+                  <p>
+                    This is a prerequisite stage all students are required to complete. You can choose the Javascript track or the Java track. Both languages allow you to develop business applications, mobile apps and desktop solutions. To load the contents of the track you selected, press the continue button and confirm.
+                  </p>
+                    <a class="btn btn-info btn-ls" href="youtube.com" target="blank">inform</a>
+              </article>
+               <article class="col-md-3 col-lg-3 col-xs-12 col-lg-12">
+               
+                  <h1>Mes etude</h1>
+                  <img src="sanfran.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236">
+                  <p>
+                    This is a prerequisite stage all students are required to complete. You can choose the Javascript track or the Java track. Both languages allow you to develop business applications, mobile apps and desktop solutions. To load the contents of the track you selected, press the continue button and confirm.
+                  </p>
+                   <a class="btn btn-info btn-ls" href="youtube.com" target="blank">inform</a>
+              </article>
+                <article class="col-md-3 col-lg-3 col-xs-12 col-lg-12">
+                 
+                  <h1>Mes etude</h1>
+
+                   <img src="sanfran.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236">
+                  <p>
+                    This is a prerequisite stage all students are required to complete. You can choose the Javascript track or the Java track. Both languages allow you to develop business applications, mobile apps and desktop solutions. To load the contents of the track you selected, press the continue button and confirm.
+                  </p>
+                  <a class="btn btn-info btn-ls" href="youtube.com" target="blank">inform</a>
+              </article>
+            </div>
+        </div>
+       </div>    
+      <script src="popper.js"></script>
+       <script src="jquery.min.js"></script>
+       <script src="bootstrap.js"></script>
+</body>
+</html>                            
+<!--<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+
+ <div class="item">
+      <img src="chicago.jpg" alt="Chicago">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>
+    </div>
+
+     <div class="carousel-inner">
+    <div class="item active">
+      <img src="la.jpg" alt="Chania">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>LA is always so much fun!</p>
+      </div>
+    </div>
+
+
+    DSC_1058.jpg
+img2.jpg
+img2.jpg
+ <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    
+    <div class="navbar-header  ">
+       <a class="navbar-brand " href="#">WebSiteName</a>
+          <ul class="nav navbar-inline navbar-left">
+           <li class="active"><a href="#">Home</a></li>
+           <li><a href="#">Page 1</a></li>
+           <li><a href="#">Page 2</a></li>
+           <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 1</a></li>
+          
+         </ul>
+       </div>
+
+       <div class="collapse navbar-collapse" id="myNavbar">
+  <div  class="navbar-form">
+            <form class="navbar-form navbar-right">
+         <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+          <button type="submit" class="btn btn-default">Submit</button>
+       </form>
+         </div>
+         
+       
+       
+    </div>
+     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+         <a class="navbar-brand" href="#">WebSiteName</a>
+        </button>
+  </div>
+</nav>
---
+++ pageAceuil.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> pageAceuil</title>
+
+	<link rel="stylesheet"  href="css/bootstrap.min.css">\
+	<link rel="stylesheet"  href="stylecss.css">
+</head>
+<body>
+	<div class="container-fluid red">
+		<h1>gwgdgdgdgdg</h1>
+	</div>
+</body>
+</html>
