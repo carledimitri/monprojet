2800799437ce0ce66d6d56e9ebfc5fbe0686dc5c
carledimitri <mouelecarl@gmail.com>
2018-01-24 18:32:35 +0100

    ajout de  formenregistrer.php

---
+++ formenregistrer.php
@@ -0,0 +1,133 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+  
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet"  href="bootstrap.min.css">
+ 
+  <link rel="stylesheet"  href="stylecss.css">
+<style type="text/css">
+    .bs-example{
+     margin: 20PX;
+    }
+  .contenair-fluid{
+  padding: 0;
+}
+</style>
+</head> 
+<body>
+	<form style="padding-left: 30px;" class="form-horizontal" action="index.php" >
+		<div class="row">
+          <div class="col-md-3"></div>
+            <div class="col-md-6">
+               <h2>connectez-vous</h2>
+                 <hr>
+            </div>
+        </div>
+        <div class="row">
+        	<div class="col-md-3"></div>
+        	<div  class="col-md-6">
+				<div class="form-group">
+				  <label class="form-horizontal" for="email" >Email adresse</label>
+				   <input class="form-control" id="email" type="email" placeholder="Enter email" name="" required>	
+				</div>
+			</div>
+		</div>	
+		<div class="row">
+          <div class="col-md-3"></div>
+        	<div  class="col-md-6">
+			  <div class="form-group">
+	   			<label for="pwd">Password:</label>
+	   		 	<input type="password" class="form-control" placeholder="Enter password" id="pwd" required>
+	   		  </div>
+	  		 </div>
+	  		</div>
+	  	   
+  			<br>
+  		<div class="row" style="padding-top: 1rem">
+           <div class="col-md-3"></div>
+             <div class="col-md-6">
+                <button  style="width: 90px;" type="submit" class="btn btn-info"><i class="fa fa-sign-in"></i>connexion</button>
+                <a class="btn btn-link" href="/password/reset">Forgot Your Password?</a>
+            </div>
+        </div>
+	</form>
+      
+    <script src="popper.js"></script>
+    <script src="jquery.min.js"></script>
+    <script src="bootstrap.js"></script>
+</body>
+</html>                            
+<!--<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+
+ <div class="item">
+      <img src="chicago.jpg" alt="Chicago">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>
+    </div>
+
+     <div class="carousel-inner">
+    <div class="item active">
+      <img src="la.jpg" alt="Chania">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>LA is always so much fun!</p>
+      </div>
+    </div>
+
+
+    DSC_1058.jpg
+img2.jpg
+img2.jpg
+ <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    
+    <div class="navbar-header  ">
+       <a class="navbar-brand " href="#">WebSiteName</a>
+          <ul class="nav navbar-inline navbar-left">
+           <li class="active"><a href="#">Home</a></li>
+           <li><a href="#">Page 1</a></li>
+           <li><a href="#">Page 2</a></li>
+           <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 1</a></li>
+          
+         </ul>
+       </div>
+
+       <div class="collapse navbar-collapse" id="myNavbar">
+  <div  class="navbar-form">
+            <form class="navbar-form navbar-right">
+         <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+          <button type="submit" class="btn btn-default">Submit</button>
+       </form>
+         </div>
+         
+       
+       
+    </div>
+     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+         <a class="navbar-brand" href="#">WebSiteName</a>
+        </button>
+  </div>
+</nav>
